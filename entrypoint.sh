ed04d9900092a1f127ae008fffd47b65dc8fc1b2
Liam Marshall <liamim@users.noreply.github.com>
2019-12-24 21:03:48 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,27 @@
 #!/bin/sh -l
+set -eu
 
-echo "hello $1"
+cd "$GITHUB_WORKSPACE" || exit
+
+echo "==> Updating apk package lists"
+apk update
+
+echo "==> Installing Clang apk package"
+apk add clang
+
+echo "==> Finding .{c,h,cpp,hpp} files"
+echo "    (exclude directories: $ignore)"
+excludes=$(echo $ignore | sed 's/ /\\\|/g')
+src=$(git ls-tree --full-tree -r HEAD | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2 | grep -ve "^\($ignore\)/")
+
+echo "==> Configuring Git author"
+git config --global user.email clang-format@wisc.space
+git config --global user.name "Clang-Format"
+
+clang-format -style=file $src
+
+echo "==> Committing style changes"
+git commit -a -m 'apply clang-format'
+
+echo "==> pushing to $BRANCH"
+git push -u origin $BRANCH
